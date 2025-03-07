02d44d7d8fe3b2788fa3c7032fbdee647d15c99b
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-03-07 21:23:52 +0100

    11. Imperative Style vs Functional Style

--- src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
+++ src/main/java/com/learnreactiveprogramming/imperative/ImperativeExample.java
@@ -1,4 +1,26 @@
 package com.learnreactiveprogramming.imperative;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ImperativeExample {
+
+    public static void main(String[] args) {
+        var nameList = List.of("alex", "ben", "chloe", "adam", "emma", "maya", "sophia", "adam", "ben", "chloe");
+
+        var newNamesList = namesGreaterThanSize(nameList, 3);
+        System.out.println("newNamesList = " + newNamesList);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> nameList, int size) {
+        return nameList
+                .parallelStream()
+                .filter(name -> name.length() > size)
+                .map(String::toUpperCase)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
 }

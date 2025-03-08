577efb4c954d054f6c12a691c48dbdc205ca374b
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-03-08 15:41:41 +0100

    13. Let's write our very first Flux and Mono

---
+++ src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -0,0 +1,38 @@
+package com.learnreactiveprogramming.service;
+
+import java.util.List;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * FluxAndMonoGeneratorService
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 08/03/2025 - 15:25
+ * @since 1.17
+ */
+public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("adam", "anna", "jack", "jenny")).log();
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("adam");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("FluxAndMonoGeneratorService.main");
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> System.out.println("Name is: " + name));
+
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(name -> System.out.println("Mono name is: " + name));
+
+    }
+
+}

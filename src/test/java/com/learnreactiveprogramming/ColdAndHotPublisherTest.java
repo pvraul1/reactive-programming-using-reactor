ba337fd2257f8a7acff4b35aa818444706427c29
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-06 11:01:56 +0200

    63. Cold Streams

--- src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
+++ src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
@@ -1,4 +1,20 @@
 package com.learnreactiveprogramming;
 
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
 public class ColdAndHotPublisherTest {
+
+    @Test
+    void coldPublisherTest() {
+        var flux = Flux.range(1, 10);
+
+        flux.subscribe(i -> System.out.println("Subscriber 1: " + i));
+
+        flux.subscribe(i -> System.out.println("Subscriber 2: " + i));
+    }
+
+
+
 }

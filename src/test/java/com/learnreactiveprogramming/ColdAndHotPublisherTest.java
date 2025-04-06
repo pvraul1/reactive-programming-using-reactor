c3ba7c42ce9a8c6db502b0c9fcd67be744d0e63a
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-06 14:49:46 +0200

    64. Hot Streams - ConnectableFlux

--- src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
+++ src/test/java/com/learnreactiveprogramming/ColdAndHotPublisherTest.java
@@ -1,8 +1,12 @@
 package com.learnreactiveprogramming;
 
 
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+
+import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
 public class ColdAndHotPublisherTest {
 
@@ -15,6 +19,19 @@ public class ColdAndHotPublisherTest {
         flux.subscribe(i -> System.out.println("Subscriber 2: " + i));
     }
 
+    @Test
+    void hotPublisherTest() {
+        var flux = Flux.range(1, 10)
+                .delayElements(Duration.ofSeconds(1));
 
+        ConnectableFlux<Integer> connectableFlux = flux.publish();
+        connectableFlux.connect();
+
+        connectableFlux.subscribe(i -> System.out.println("Subscriber 1: " + i));
+        delay(4000);
+
+        connectableFlux.subscribe(i -> System.out.println("Subscriber 2: " + i));
+        delay(2000);
+    }
 
 }

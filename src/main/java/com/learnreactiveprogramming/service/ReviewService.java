3a0d49fae332a8c6e5fe86184dc93d0a2ff63d5b
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-20 21:37:30 +0200

    71. Build the non-blocking Review RestClient

--- src/main/java/com/learnreactiveprogramming/service/ReviewService.java
+++ src/main/java/com/learnreactiveprogramming/service/ReviewService.java
@@ -1,11 +1,18 @@
 package com.learnreactiveprogramming.service;
 
 import com.learnreactiveprogramming.domain.Review;
+import java.util.List;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 
-import java.util.List;
-
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final WebClient webClient;
 
     public  List<Review> retrieveReviews(long movieInfoId){
 
@@ -13,7 +20,20 @@ public class ReviewService {
                 new Review(2L, movieInfoId, "Excellent Movie", 9.0));
     }
 
-    public Flux<Review> retrieveReviewsFlux(long movieInfoId){
+    public Flux<Review> retrieveReviewsFlux_RestClient(long movieInfoId) {
+
+        var uri = UriComponentsBuilder.fromUriString("/v1/reviews")
+                .queryParam("movieInfoId", movieInfoId)
+                .buildAndExpand()
+                .toUriString();
+
+        return webClient.get().uri(uri)
+                .retrieve()
+                .bodyToFlux(Review.class)
+                .log();
+    }
+
+    public Flux<Review> retrieveReviewsFlux(long movieInfoId) {
 
         var reviewsList = List.of(new Review(1L,movieInfoId, "Awesome Movie", 8.9),
                 new Review(2L, movieInfoId, "Excellent Movie", 9.0));
